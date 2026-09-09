49779b62ef29afc2a757e1d11206640ece901a3f
WhoBuscusYT <jj99118725@gmail.com>
2026-09-09 17:16:02 -0400

    Updated to Z# v1.0.2.4

--- Main.js
+++ Main.js
@@ -1,6 +1,6 @@
 /* Change the version here when a new Z# release is published. */
 window.ZSharpSite = Object.freeze({
-  version: '1.0.2.3',
+  version: '1.0.2.4',
   installerRoot: 'https://www.zsharp.zombieos.com/assets/download/installers',
   testAppUrl: 'https://www.zsharp.zombieos.com/assets/download/ZSharp-Test-App.zapp',
   testGameUrl: 'https://www.zsharp.zombieos.com/assets/download/ZSharpGameTest.zgame',
--- update.js
+++ update.js
@@ -1,45 +1,45 @@
 {
   "schema": 1,
-  "latestVersion": "1.0.2.3",
+  "latestVersion": "1.0.2.4",
   "channel": "stable",
   "download": {
     "url": "https://www.zsharp.zombieos.com/assets/download/ZVM-LATEST.zip",
-    "sha256": "88f0f96150f4e11f3f1a5e5541d3c2e8c4ef01f7fc7ee7f12b45e30715b12194",
-    "size": 44183018
+    "sha256": "b394d0e5e527c4273d464bb5946620f6f35415234a33be6eaff97729cfbb8ca2",
+    "size": 44297970
   },
   "platforms": {
     "windows-x86_64": {
       "path": "runtimes/windows-x86_64/zsharp.exe",
-      "sha256": "9c5c6bc61a240e53ac69f290f3710757b263769a80d3ebdcc7b920cc66ea30b9",
-      "size": 3050496
+      "sha256": "272c937daa4f4a5241e54441e0f15e8db5280a93e06553d9e72a12f866c458b7",
+      "size": 3061248
     },
     "windows-aarch64": {
       "path": "runtimes/windows-aarch64/zsharp.exe",
-      "sha256": "7825e20d29bf3baa7cb3bdbe20412948cbef9f4586358a757ec375ff14af74bb",
-      "size": 2865152
+      "sha256": "fdc6997eaef174b2afc7f228fc0275c22ee09fc4cfff0f58c89f00aab56a3683",
+      "size": 2877440
     },
     "linux-x86_64": {
       "path": "runtimes/linux-x86_64/zsharp",
-      "sha256": "2feeed088e8508d74307cb2461ff567e0a198ed5252913dd019acaae84080015",
-      "size": 4968072
+      "sha256": "45150ed56c7c7598ac20a7471e9898ff7a5f5020e09691206ac54a87c3d90f83",
+      "size": 4980776
     },
     "linux-aarch64": {
       "path": "runtimes/linux-aarch64/zsharp",
-      "sha256": "49e9ba49c306afa03967d88c39b948cc6892e83bc50b1426c79c268eabba53cc",
-      "size": 4667008
+      "sha256": "0a44aef2dcd671cded7b5e043ad64d8d9266c783e6b24d21e02875b59f6a0cdb",
+      "size": 4733160
     },
     "macos-x86_64": {
       "path": "runtimes/macos-x86_64/zsharp",
-      "sha256": "b0a37333a32561b4c30e1ba8df33bcbe1098412a313c60b2f25854012e221717",
-      "size": 3514920,
+      "sha256": "ca45f366b8636299d12660f7f265bd150014ad554affbd1c7dac440790c001ae",
+      "size": 3527592,
       "supportPath": "runtimes/macos-x86_64/libMoltenVK.dylib",
       "supportSha256": "aef00b13bcc808adf15b85bef9ae67393d92be7ed5dfe41cad16fa809e4a4c5f",
       "supportSize": 10925552
     },
     "macos-aarch64": {
       "path": "runtimes/macos-aarch64/zsharp",
-      "sha256": "2d083fd26029e56336d38be07758d7ad970cdd73f17dd781ac44184b9d179794",
-      "size": 3265112,
+      "sha256": "9822763b5fb5d958af6ab077b5e1b465e2d28136d99d9b5f828c6354ab69af17",
+      "size": 3265496,
       "supportPath": "runtimes/macos-aarch64/libMoltenVK.dylib",
       "supportSha256": "aef00b13bcc808adf15b85bef9ae67393d92be7ed5dfe41cad16fa809e4a4c5f",
       "supportSize": 10925552
